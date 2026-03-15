c980a3071fc4826994b7d8513bedf2d0976642d3
Xin Long <lucien.xin@gmail.com>
2026-03-15 19:10:22 -0400

    quic: provide quic.h header files for kernel and userspace
    This commit adds quic.h to include/uapi/linux, providing the necessary
    definitions for the QUIC socket API. Exporting this header allows both
    user space applications and kernel subsystems to access QUIC-related
    control messages, socket options, and event/notification interfaces.
    
    Since kernel_get/setsockopt() is no longer available to kernel consumers,
    a corresponding internal header, include/linux/quic.h, is added. This
    exposes quic_do_get/setsockopt() to handle QUIC socket options directly
    for kernel subsystems.
    
    Detailed descriptions of these structures are available in [1], and will
    be also provided when adding corresponding socket interfaces in the
    later patches.
    
    [1] https://datatracker.ietf.org/doc/html/draft-lxin-quic-socket-apis
    
    Signed-off-by: Tyler Fanelli <tfanelli@redhat.com>
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Signed-off-by: Thomas Dreibholz <dreibh@simula.no>
    Signed-off-by: Xin Long <lucien.xin@gmail.com>
    Acked-by: Paolo Abeni <pabeni@redhat.com>

---
+++ include/linux/quic.h
@@ -0,0 +1,24 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/* QUIC kernel implementation
+ * (C) Copyright Red Hat Corp. 2023
+ *
+ * This file is part of the QUIC kernel implementation
+ *
+ * Written or modified by:
+ *    Xin Long <lucien.xin@gmail.com>
+ */
+
+#ifndef _LINUX_QUIC_H
+#define _LINUX_QUIC_H
+
+#include <linux/sockptr.h>
+#include <uapi/linux/quic.h>
+
+struct sock;
+
+int quic_do_setsockopt(struct sock *sk, int optname, sockptr_t optval,
+		       unsigned int optlen);
+int quic_do_getsockopt(struct sock *sk, int optname, sockptr_t optval,
+		       sockptr_t optlen);
+
+#endif
